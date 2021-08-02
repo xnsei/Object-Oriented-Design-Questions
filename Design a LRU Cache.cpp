2d6328dcc01090d580df4946181144e39701b957
Tanmay Banage <75128196+xnsei@users.noreply.github.com>
2021-08-02 18:05:50 +0530

    Update Design a LRU Cache.cpp

--- Design a LRU Cache.cpp
+++ Design a LRU Cache.cpp
@@ -1,7 +1,7 @@
 class Node{
 public:
     int key, value;
-    Node *next, *prev;
+    Node* prev, *next;
     
     Node(int k, int v){
         key = k;
@@ -11,121 +11,80 @@ public:
     }
 };
 
-class DoublyLinkedList{
-    Node *head, *tail;
-    
-    bool isEmpty(){
-        return head == NULL;
-    }
-public:
-    DoublyLinkedList(){
-        head = NULL;
-        tail = head;
-    }
-    
-    Node* addFirst(int key, int value){
-        Node* curr = new Node(key, value);
-        if(head == NULL){
-            head = tail = NULL;
-        }
-        else{
-            curr->next = head;
-            head->prev = curr;
-            head = curr;
-        }
-        return head;
-    }
-    
-    void moveToHead(Node* curr){
-           
-        if(curr == head){
-            return;
-        }    
-        else if(curr == tail){
-            tail = curr->prev;
-            tail->next = NULL;
-        }
-        
-        curr->next = head;
-        curr->prev = NULL;
-        head->prev = curr;
-        head = curr;
-    }
-    
-    void removeLast(){
-        if(isEmpty())return;
-        
-        if(head == tail){
-            delete(head);
-            head = tail = NULL;
-        }
-        else{
-            Node* temp = tail;
-            tail =  tail->prev;
-            tail->next = NULL;
-            delete(temp);
-        }
-    }
-    
-    Node* getTail(){
-        return tail;
-    }
-};
-
 
 class LRUCache {
     
+    Node* head, *tail;
     int capacity, size;
-  DoublyLinkedList *pageList;
-  map<int, Node*> pageMap;
-
-  public:
+    unordered_map<int, Node*> m;
+    
+    void removeNode(Node* curr){
+        if(curr->prev!=NULL){
+            curr->prev->next = curr->next;
+        }
+        else{
+            head = curr->next;
+        }
+        if(curr->next != NULL){
+            curr->next->prev = curr->prev;
+            
+        }
+        else{
+            tail = curr->prev;
+        }
+        curr->prev=NULL;
+        curr->next=NULL;
+    }
+    
+    void addFirst(Node* curr){
+        if (head == NULL)
+        {
+            head = tail = curr;
+        }
+        else{
+        head->prev = curr;
+        curr->next = head;
+        head = curr;
+        }
+    }  
+    
+public:
     LRUCache(int capacity) {
-      this->capacity = capacity;
-      size = 0;
-        pageList = new DoublyLinkedList();
-        pageMap = map<int, Node*>();
+        head = NULL;
+        tail = head;
+        this->capacity = capacity;
+        size = 0;
     }
-
+    
     int get(int key) {
-        if(pageMap.find(key)==pageMap.end()) {
-          return -1;
-        }
-        int val = pageMap[key]->value;
-
-        // move the page to front
-        pageList->moveToHead(pageMap[key]);
-        return val;
+        if (m.find(key)==m.end())
+            return -1;
+        Node *curr=m[key];
+        removeNode(curr);
+        addFirst(curr);
+        //insert the node at first
+        
+        return curr->value;
     }
-
+    
     void put(int key, int value) {
-      if(pageMap.find(key)!=pageMap.end()) {
-          // if key already present, update value and move page to head
-          pageMap[key]->value = value;
-          pageList->moveToHead(pageMap[key]);
-          return;
-      }
-
-        if(size == capacity) {
-          // remove rear page
-          int k = pageList->getTail()->key;
-          pageMap.erase(k);
-          pageList->removeLast();
-          size--;
+        if(m.find(key) != m.end()){
+            m[key]->value = value;
+            removeNode(m[key]);
+            addFirst(m[key]);
         }
-
-        // add new page to head to Queue
-        Node *page = pageList->addFirst(key, value);
-        size++;
-        pageMap[key] = page;
-    }
-
-    ~LRUCache() {
-      map<int, Node*>::iterator i1;
-      for(i1=pageMap.begin();i1!=pageMap.end();i1++) {
-          delete i1->second;
-      }
-      delete pageList;
+        else{
+            Node* curr = new Node(key, value);
+            if(size == capacity){
+                int key = tail->key;
+                m.erase(key);
+                size--;
+                removeNode(tail);
+            }
+            addFirst(curr);
+            m[key] = curr;
+            size++;
+        }
     }
 };
 
